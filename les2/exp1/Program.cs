864cac21a2d76e9c4caa1f9f275bd2208230fea0
kutuzoffmoscow <kutuzoffmoscow@gmail.com>
2023-01-18 17:39:05 +0300

    добавил описание

--- les2/exp1/Program.cs
+++ les2/exp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void n (int collection)
+{
+    string s = collection.ToString();
+    if(s.Length >= 2)
+    {
+        Console.WriteLine(s[1]);
+    }
+    else
+    {
+        Console.WriteLine("ввидите 3хзначное число");
+    }
+    
+}
+
+int collection = 562;
+n (collection);
