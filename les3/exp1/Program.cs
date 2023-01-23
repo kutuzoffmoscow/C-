8eb3b79b3435bd8d56c395e5e322cea546e5b16f
kutuzoffmoscow <kutuzoffmoscow@gmail.com>
2023-01-23 15:59:55 +0300

    решение задачи

---
+++ les3/exp1/Program.cs
@@ -0,0 +1,21 @@
+﻿void Pal(int number)
+{
+    if (number < 10000 || number > 100000)
+    {
+        Console.WriteLine("Число должно быть больше 9999 и меньше 100000.");
+    }
+    else if (number / 10000 == number % 100 % 10
+        && number / 1000 % 10 == number % 100 / 10)
+    {
+        Console.WriteLine("Палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Не палиндром");
+    }
+}
+
+Console.WriteLine("Введите пятизначное число для проверки на палиндром: ");
+int number = System.Convert.ToInt32(Console.ReadLine());
+
+Pal(number);
