d716c76cc9d67c2e6f4b1074e94dd9a6a1f7261d
kutuzoffmoscow <kutuzoffmoscow@gmail.com>
2023-01-25 11:28:40 +0300

    решение задачи

---
+++ les4/exp1/Program.cs
@@ -0,0 +1,17 @@
+﻿int digitSum(int number)
+{
+    int absNumber = Math.Abs(number);
+    string tempStr = System.Convert.ToString(absNumber);
+    int result = 0;
+
+    for (int i = 0; i <= tempStr.Length - 1; i++)
+    {
+        result += (int)Char.GetNumericValue(tempStr[i]);
+    }
+    return result;
+}
+
+Random rnd = new Random();
+int number = rnd.Next(0,100000);
+Console.WriteLine($"Число: {number}");
+Console.WriteLine($"Сумма: {digitSum(number)}");
